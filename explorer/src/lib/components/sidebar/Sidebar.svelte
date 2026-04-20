49fed0d2e5f3ac2aa140bffd1f1594ace0caf152
Viktor Bengtsson <viktor@vkbn.ltd>
2026-04-20 22:19:34 +0100

    chore(explorer): drop '2' suffix from site brand in nav + sidebar

--- explorer/src/lib/components/sidebar/Sidebar.svelte
+++ explorer/src/lib/components/sidebar/Sidebar.svelte
@@ -20,7 +20,7 @@
 
 <div class="absolute top-6 left-6 z-50 flex flex-row items-center space-x-4 pl-1 pb-4">
     <button class="cursor-pointer" onclick={toggleDrawer}><img src="/logo.svg" alt="Logo" class="h-8" /></button>
-    <span class="text-2xl font-bold transition-opacity duration-300 ease-in-out" class:opacity-0={!toggleControls} class:opacity-100={toggleControls}>Behovskartan 2</span>
+    <span class="text-2xl font-bold transition-opacity duration-300 ease-in-out" class:opacity-0={!toggleControls} class:opacity-100={toggleControls}>Behovskartan</span>
 </div>
 
 <nav class="fixed top-0 left-0 h-full w-72 2xl:w-80 flex flex-col items-start px-5 pt-6 pb-4 bg-slate-100 transition-transform duration-300 ease-in-out z-30 {toggleControls ? 'translate-x-0' : '-translate-x-full'}">
